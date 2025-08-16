3db3585e20f1dd7a757ef686f0211e254545f02d
mlawinski99 <mlawinskiv1@gmail.com>
2025-08-16 18:48:46 +0200

    chatdb context dbsets added

--- Chatter.Messages.DataAccess/DbContexts/ChatDbContext.cs
+++ Chatter.Messages.DataAccess/DbContexts/ChatDbContext.cs
@@ -1,4 +1,5 @@
 using Chatter.MessagesDataAccess.DbEntitiesConfigurations;
+using Chatter.MessagesDomain;
 using Chatter.Shared.DataAccessTypes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
@@ -7,6 +8,9 @@ namespace Chatter.MessagesDataAccess.DbContexts;
 
 public class ChatDbContext : BaseDbContext
 {
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<ChatMember> ChatMembers { get; set; }
+    public DbSet<Chat> Chats { get; set; }
     public ChatDbContext(DbContextOptions<ChatDbContext> options, 
         IEnumerable<IInterceptor> interceptors) : base(options, interceptors)
     {
